1e4ea84799c48617fbfc99dd773d3b9db8a6c477
Dean <imadeureadis@gmail.com>
2024-01-28 14:10:18 +0100

    successful logging into database but no archival or proc propagation

--- dagster/callables/customer_etl.py
+++ dagster/callables/customer_etl.py
@@ -1,5 +1,9 @@
+import gzip
+import hashlib
+import json
 import logging
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 from psycopg2.pool import SimpleConnectionPool
 
@@ -30,6 +34,10 @@ def get_connection():
     return connection_pool.getconn()
 
 
+def release_connection(conn):
+    connection_pool.putconn(conn)
+
+
 def connect_to_postgres():
     return get_connection()
 
@@ -55,10 +63,162 @@ def create_processed_data_log_table(connection, schema_name):
     connection.commit()
 
 
+def extract_data(file_path):
+    if not file_path:
+        return []
+    # Extract raw_data from a gzipped JSON file
+    with gzip.open(file_path, "rt") as file:
+        data = [json.loads(line) for line in file]
+    return data
+
+
+def transform_and_validate_customers(customers_data):
+    # Transform and validate customer raw_data
+    # Ensure required fields are populated, id is unique, and other constraints
+    unique_ids = set()
+
+    for customer in customers_data:
+        if 'id' not in customer or 'first_name' not in customer or 'last_name' not in customer or 'email' not in customer:
+            # Log or handle missing required fields
+            continue
+
+        if customer['id'] in unique_ids:
+            # Log or handle duplicate ids
+            continue
+
+        # Additional validation logic (e.g., email format, date_of_birth format)
+
+        # Update last_change timestamp
+        customer['last_change'] = datetime.utcnow().isoformat()
+
+        unique_ids.add(customer['id'])
+
+    return customers_data
+
+
+def extract_actual_date(date_str):
+    return datetime.strptime(date_str.replace("date=", ""), "%Y-%m-%d").date()
+
+
+# Extract the actual hour from the "hour=00" format
+def extract_actual_hour(hour_str):
+    return int(hour_str.replace("hour=", ""))
+
+
+def log_processed_customers(connection, date, hour, customer_ids):
+    actual_date = extract_actual_date(date)
+    actual_hour = extract_actual_hour(hour)
+    with connection.cursor() as cursor:
+        for customer_id in customer_ids:
+            cursor.execute("""
+                INSERT INTO processed_data_log (date, hour, customer_id) 
+                VALUES (%s, %s, %s);
+            """, (actual_date, actual_hour, customer_id))
+    connection.commit()
+
+
+def load_data(data, dataset_type, date, hour):
+    logger.debug(f"Loading data for {dataset_type}")
+    # Create the corresponding subdirectories in processed_data
+    output_dir = os.path.join(PROCESSED_DATA_PATH, date, hour)
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Determine the appropriate file extension based on dataset_type
+    if dataset_type.endswith(".json.gz"):
+        file_extension = ".json.gz"
+    elif dataset_type.endswith(".json"):
+        file_extension = ".json"
+    else:
+        raise ValueError(f"Unsupported file extension in dataset_type: {dataset_type}")
+
+    # Remove the existing extension if present
+    dataset_type_without_extension, _ = dataset_type.split(".", 1)
+
+    # Load processed data to the new location only if the dataset is not empty
+    if data:
+        # Use gzip compression if the file extension is .json.gz
+        open_func = gzip.open if file_extension == ".json.gz" else open
+        # Construct the output path
+        output_path = os.path.join(output_dir, f"{dataset_type_without_extension}{file_extension}")
+
+        with open_func(output_path, "wt") as file:
+            for record in data:
+                json.dump(record, file)
+                file.write("\n")
+    else:
+        logger.debug(f"Skipping loading for empty dataset: {dataset_type}")
+
+
+def archive_and_delete(file_path, dataset_type, date, hour, archive_path):
+    archive_file = dataset_type
+    archive_file_path = os.path.join(archive_path, date, hour, archive_file)
+
+    # Create the archive directory if it doesn't exist
+    os.makedirs(os.path.dirname(archive_file_path), exist_ok=True)
+
+    # Archive the file
+    os.rename(file_path, archive_file_path)
+    logger.debug(f"File archived: {archive_file_path}")
+
+
+def process_hourly_data(connection, date, hour, available_datasets):
+    print(date, hour, len(available_datasets), available_datasets)
+    dataset_paths = {dataset: os.path.join(RAW_DATA_PATH, f"{date}", f"{hour}", f"{dataset}")
+                     for dataset in available_datasets}
+    print("Dataset Paths:", dataset_paths)
+
+    # Extract raw_data
+    customers_data = extract_data(dataset_paths.get("customers.json.gz", ""))
+
+    # Transform and validate raw_data
+    transformed_customers = transform_and_validate_customers(customers_data)
+
+    # Load processed raw_data
+    load_data(transformed_customers, "customers.json.gz", date, hour)
+
+    # Log processed customers
+    customer_ids = [customer["id"] for customer in transformed_customers]
+    log_processed_customers(connection, date, hour, customer_ids)
+
+    # Archive and delete the original files
+    for dataset_type, dataset_path in dataset_paths.items():
+        # print("Processing dataset:", dataset_type, "Path:", dataset_path)
+        archive_and_delete(dataset_path, dataset_type, date, hour, ARCHIVED_DATA_PATH)
+    logger.debug("Processing completed.")
+
+
 def process_all_data():
     schema_name = "data"
     connection = connect_to_postgres()
     create_processed_data_log_table(connection, schema_name)
+    # Get a sorted list of date folders
+    try:
+        date_folders = os.listdir(RAW_DATA_PATH)
+        date_folders.sort()
+        # Process all available raw_data
+        for date_folder in date_folders:
+            date_path = os.path.join(RAW_DATA_PATH, date_folder)
+
+            # Get a sorted list of hour folders
+            hour_folders = os.listdir(date_path)
+            hour_folders.sort()
+
+            for hour_folder in hour_folders:
+                hour_path = os.path.join(date_path, hour_folder)
+
+                available_datasets = [filename for filename in os.listdir(hour_path) if filename.startswith("customers")
+                                      and filename.endswith((".json", ".json.gz"))]
+
+                if available_datasets:
+                    process_hourly_data(connection, date_folder, hour_folder, available_datasets)
+                else:
+                    logger.warning(f"No datasets found for {date_folder}/{hour_folder}")
+
+        #
+        # # Clean up empty directories in raw_data after processing
+        # cleanup_empty_directories(RAW_DATA_PATH)
+    finally:
+        release_connection(connection)
 
 
 def main():
