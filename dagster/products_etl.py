79bf24dfb51e3c48bc265df2d14c6ac215dfa203
Dean <imadeureadis@gmail.com>
2024-01-30 09:45:51 +0100

    add healthchecks, products table and products pipeline

--- dagster/products_etl.py
+++ dagster/products_etl.py
@@ -1,6 +1,246 @@
+import gzip
+import json
+import logging
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+from psycopg2.pool import SimpleConnectionPool
+import jsonschema
+from jsonschema import validate
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+RAW_DATA_PATH = '/opt/dagster/app/raw_data'
+PROCESSED_DATA_PATH = '/opt/dagster/app/processed_data'
+ARCHIVED_DATA_PATH = '/opt/dagster/app/archived_data'
 
 
+# Helper functions
+# todo turn on schema enforcement
+# todo call processing statistics
+# TODO schema validation json
+# TODO handle missing fields and duplicate skus in transformations
+# TODO add orchestration / workflows for different arrivals
+# TODO add tests
+# TODO connect everything to minio
+# TODO log records in database for erasure requests and have single source of truth
+# TODO prevent duplicates from loading
+# todo forget minio and loading directly in container, use volume mounts instead
 
+
+connection_pool = SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+)
+
+
+def get_connection():
+    return connection_pool.getconn()
+
+
+def release_connection(conn):
+    connection_pool.putconn(conn)
+
+
+def connect_to_postgres():
+    return get_connection()
+
+
+def log_processing_statistics(connection, date, hour, dataset_type, record_count, processing_time):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT INTO data.processing_statistics (date, hour, dataset_type, record_count, processing_time) 
+            VALUES (%s, %s, %s, %s, %s);
+        """, (date, hour, dataset_type, record_count, processing_time))
+    connection.commit()
+
+
+def extract_data(file_path):
+    if not file_path:
+        return []
+
+    # Extract raw_data from a gzipped JSON file
+    with gzip.open(file_path, "rt") as file:
+        data = [json.loads(line) for line in file]
+    return data
+
+
+def transform_and_validate_products(products_data):
+    # Load the JSON schema
+    with open("products_schema.json", "r") as schema_file:
+        schema = json.load(schema_file)
+
+    # Validate each product record against the schema
+    for product in products_data:
+        try:
+            validate(instance=product, schema=schema)
+        except jsonschema.exceptions.ValidationError as e:
+            # Log or handle validation errors
+            print(f"Validation error for product: {e}")
+
+    # Update last_change timestamp
+    for product in products_data:
+        product['last_change'] = datetime.utcnow().isoformat()
+
+    return products_data
+
+
+def load_data(data, dataset_type, date, hour):
+    logger.info(f"Loading data {data} for {dataset_type} {date} {hour}")
+    # Create the corresponding subdirectories in processed_data
+    output_dir = os.path.join(PROCESSED_DATA_PATH, date, hour)
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Determine the appropriate file extension based on dataset_type
+    if dataset_type.endswith(".json.gz"):
+        file_extension = ".json.gz"
+    elif dataset_type.endswith(".json"):
+        file_extension = ".json"
+    else:
+        raise ValueError(f"Unsupported file extension in dataset_type: {dataset_type}")
+
+    # Remove the existing extension if present
+    dataset_type_without_extension, _ = dataset_type.split(".", 1)
+
+    # Load processed data to the new location only if the dataset is not empty
+    if data:
+        # Use gzip compression if the file extension is .json.gz
+        open_func = gzip.open if file_extension == ".json.gz" else open
+        # Construct the output path
+        output_path = os.path.join(output_dir, f"{dataset_type_without_extension}{file_extension}")
+
+        with open_func(output_path, "wt") as file:
+            for record in data:
+                json.dump(record, file)
+                file.write("\n")
+    else:
+        logger.debug(f"Skipping loading for empty dataset: {dataset_type}")
+
+
+def extract_actual_date(date_str):
+    return datetime.strptime(date_str.replace("date=", ""), "%Y-%m-%d").date()
+
+
+# Extract the actual hour from the "hour=00" format
+def extract_actual_hour(hour_str):
+    return int(hour_str.replace("hour=", ""))
+
+
+def log_processed_products(connection, date, hour, skus, names, prices, categories, popularities):
+    actual_date = extract_actual_date(date)
+    actual_hour = extract_actual_hour(hour)
+    with connection.cursor() as cursor:
+        for sku, name, price, category, popularity in zip(skus, names, prices, categories, popularities):
+            # Check if the record already exists
+            cursor.execute("""
+                SELECT COUNT(*) FROM data.products
+                WHERE date = %s AND hour = %s AND sku = %s;
+            """, (actual_date, actual_hour, sku))
+
+            count = cursor.fetchone()[0]
+            if count == 0:
+                # Record doesn't exist, insert it
+                cursor.execute("""
+                    INSERT INTO data.products (sku, name, price, category, popularity, date, hour)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s);
+                """, (sku, name, price, category, popularity, actual_date, actual_hour))
+            else:
+                # Record already exists, log or handle accordingly
+                logger.info(f"Record for sku {sku} at {actual_date} {actual_hour} already exists.")
+    connection.commit()
+
+
+def archive_and_delete(file_path, dataset_type, date, hour, archive_path):
+    archive_file = dataset_type
+    archive_file_path = os.path.join(archive_path, date, hour, archive_file)
+
+    # Create the archive directory if it doesn't exist
+    os.makedirs(os.path.dirname(archive_file_path), exist_ok=True)
+
+    # Archive the file
+    os.rename(file_path, archive_file_path)
+    logger.debug(f"File archived: {archive_file_path}")
+
+
+def process_hourly_data(connection, date, hour, available_datasets):
+    print(date, hour, len(available_datasets), available_datasets)
+    dataset_paths = {dataset: os.path.join(RAW_DATA_PATH, f"{date}", f"{hour}", f"{dataset}")
+                     for dataset in available_datasets}
+    print("Dataset Paths:", dataset_paths)
+
+    # Extract raw_data
+    products_data = extract_data(dataset_paths.get("products.json.gz", ""))
+    print("Number of products:", len(products_data))
+
+    # Transform and validate raw_data
+    transformed_products = transform_and_validate_products(products_data)
+
+    # Load processed raw_data
+    load_data(transformed_products, "products.json.gz", date, hour)
+
+    # Log processed products
+    skus = [product["sku"] for product in transformed_products]
+    names = [product["name"] for product in transformed_products]
+    prices = [product["price"] for product in transformed_products]
+    categories = [product["category"] for product in transformed_products]
+    popularities = [product["popularity"] for product in transformed_products]
+    log_processed_products(connection, date, hour, skus, names, prices, categories, popularities)
+
+    # Archive and delete the original files
+    for dataset_type, dataset_path in dataset_paths.items():
+        print("Processing dataset:", dataset_type, "Path:", dataset_path)
+        archive_and_delete(dataset_path, dataset_type, date, hour, ARCHIVED_DATA_PATH)
+    logger.debug("Processing completed.")
+
+
+def cleanup_empty_directories(directory):
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            if not os.listdir(dir_path):
+                os.rmdir(dir_path)
+                logger.debug(f"Empty directory deleted: {dir_path}")
+
+
+def process_all_data():
+    connection = connect_to_postgres()
+    # Get a sorted list of date folders
+    try:
+        date_folders = os.listdir(RAW_DATA_PATH)
+        date_folders.sort()
+        print("Date Folders:", date_folders)
+        # Process all available raw_data
+        for date_folder in date_folders:
+            date_path = os.path.join(RAW_DATA_PATH, date_folder)
+
+            # Get a sorted list of hour folders
+            hour_folders = os.listdir(date_path)
+            hour_folders.sort()
+            print(f"Hour Folders for {date_folder}:", hour_folders)
+
+            for hour_folder in hour_folders:
+                hour_path = os.path.join(date_path, hour_folder)
+
+                available_datasets = [filename for filename in os.listdir(hour_path) if filename.startswith("products")
+                                      and filename.endswith((".json", ".json.gz"))]
+
+                if available_datasets:
+                    process_hourly_data(connection, date_folder, hour_folder, available_datasets)
+                else:
+                    logger.warning(f"No datasets found for {date_folder}/{hour_folder}")
+
+        # Clean up empty directories in raw_data after processing
+        cleanup_empty_directories(RAW_DATA_PATH)
+    finally:
+        release_connection(connection)
 
 
 def main():
--- dagster/sql-scripts/init.sql
+++ dagster/sql-scripts/init.sql
@@ -26,3 +26,14 @@ CREATE TABLE IF NOT EXISTS data.transactions (
     customer_id INTEGER NOT NULL,
     processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS data.products (
+    sku INTEGER PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    popularity DOUBLE PRECISION CHECK (popularity > 0) NOT NULL,
+    date DATE NOT NULL,
+    hour INTEGER NOT NULL,
+    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
