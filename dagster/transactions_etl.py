1b8815dc2a2f053d327ab53d330da1ceea19ccaa
Dean <imadeureadis@gmail.com>
2024-01-31 10:58:30 +0100

    refactor transactions_etl

--- dagster/transactions_etl.py
+++ dagster/transactions_etl.py
@@ -1,13 +1,14 @@
 import traceback
-import gzip
 import json
 import logging
 import os
 from datetime import datetime
 from dotenv import load_dotenv
-from psycopg2.pool import SimpleConnectionPool
 import jsonschema
 from jsonschema import validate
+from common import archive_and_delete, extract_actual_date, extract_actual_hour, connect_to_postgres, extract_data, load_data, log_processing_statistics, cleanup_empty_directories
+import psycopg2
+
 
 load_dotenv()
 
@@ -21,33 +22,6 @@ INVALID_RECORDS_TABLE = 'data.invalid_transactions'
 TRANSACTIONS_SCHEMA_FILE = "transactions_schema.json"
 
 
-def create_connection_pool():
-    return SimpleConnectionPool(
-        minconn=1,
-        maxconn=10,
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-    )
-
-
-connection_pool = create_connection_pool()
-
-
-def get_connection():
-    return connection_pool.getconn()
-
-
-def release_connection(conn):
-    connection_pool.putconn(conn)
-
-
-def connect_to_postgres():
-    return get_connection()
-
-
 def log_invalid_transaction(connection, transaction, error_message, date, hour):
     actual_date = extract_actual_date(date)
     actual_hour = extract_actual_hour(hour)
@@ -57,71 +31,6 @@ def log_invalid_transaction(connection, transaction, error_message, date, hour):
             VALUES (%s, %s, %s, %s, %s);
         """, (actual_date, actual_hour, transaction.get("transaction_id"), transaction.get("customer_id"), error_message))
     connection.commit()
-
-
-def log_processing_statistics(connection, date, hour, dataset_type, record_count, processing_time):
-    actual_date = extract_actual_date(date)
-    actual_hour = extract_actual_hour(hour)
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            INSERT INTO data.processing_statistics (record_date, record_hour, dataset_type, record_count, processing_time) 
-            VALUES (%s, %s, %s, %s, %s);
-        """, (actual_date, actual_hour, dataset_type, record_count, processing_time))
-    connection.commit()
-
-
-def extract_data(file_path):
-    if not file_path:
-        return []
-
-    # Extract raw_data from a gzipped JSON file
-    with gzip.open(file_path, "rt") as file:
-        data = [json.loads(line) for line in file]
-    return data
-
-
-def load_data(data, dataset_type, date, hour):
-    logger.info(f"Loading data {data} for {dataset_type} {date} {hour}")
-    # Create the corresponding subdirectories in processed_data
-    output_dir = os.path.join(PROCESSED_DATA_PATH, date, hour)
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Determine the appropriate file extension based on dataset_type
-    if dataset_type.endswith(".json.gz"):
-        file_extension = ".json.gz"
-    elif dataset_type.endswith(".json"):
-        file_extension = ".json"
-    else:
-        raise ValueError(f"Unsupported file extension in dataset_type: {dataset_type}")
-
-    # Remove the existing extension if present
-    dataset_type_without_extension, _ = dataset_type.split(".", 1)
-
-    # Load processed data to the new location only if the dataset is not empty
-    if data:
-        # Use gzip compression if the file extension is .json.gz
-        open_func = gzip.open if file_extension == ".json.gz" else open
-        # Construct the output path
-        output_path = os.path.join(output_dir, f"{dataset_type_without_extension}{file_extension}")
-
-        with open_func(output_path, "wt") as file:
-            for record in data:
-                json.dump(record, file)
-                file.write("\n")
-    else:
-        logger.debug(f"Skipping loading for empty dataset: {dataset_type}")
-
-
-def archive_and_delete(file_path, dataset_type, date, hour, archive_path):
-    archive_file = dataset_type
-    archive_file_path = os.path.join(archive_path, date, hour, archive_file)
-
-    # Create the archive directory if it doesn't exist
-    os.makedirs(os.path.dirname(archive_file_path), exist_ok=True)
-
-    # Archive the file
-    os.rename(file_path, archive_file_path)
-    logger.debug(f"File archived: {archive_file_path}")
 
 
 def is_existing_customer(connection, customer_id):
@@ -221,15 +130,6 @@ def transform_and_validate_transactions(connection, transactions_data, date, hou
             continue
 
     return valid_transactions
-
-
-def extract_actual_date(date_str):
-    return datetime.strptime(date_str.replace("date=", ""), "%Y-%m-%d").date()
-
-
-# Extract the actual hour from the "hour=00" format
-def extract_actual_hour(hour_str):
-    return int(hour_str.replace("hour=", ""))
 
 
 def get_delivery_address_by_transaction_id(connection, transaction_id):
@@ -353,7 +253,7 @@ def process_hourly_data(connection, date, hour, available_datasets):
     transformed_transactions = transform_and_validate_transactions(connection, transactions_data, date, hour)
 
     # Load processed raw_data
-    load_data(transformed_transactions, "transactions.json.gz", date, hour)
+    load_data(transformed_transactions, "transactions.json.gz", date, hour, PROCESSED_DATA_PATH)
 
     # Log processed transactions
     transaction_ids = [transaction["transaction_id"] for transaction in transformed_transactions]
@@ -373,15 +273,6 @@ def process_hourly_data(connection, date, hour, available_datasets):
         print("Processing dataset:", dataset_type, "Path:", dataset_path)
         archive_and_delete(dataset_path, dataset_type, date, hour, ARCHIVED_DATA_PATH)
     logger.debug("Processing completed.")
-
-
-def cleanup_empty_directories(directory):
-    for root, dirs, files in os.walk(directory, topdown=False):
-        for dir_name in dirs:
-            dir_path = os.path.join(root, dir_name)
-            if not os.listdir(dir_path):
-                os.rmdir(dir_path)
-                logger.debug(f"Empty directory deleted: {dir_path}")
 
 
 def process_all_data():
@@ -413,8 +304,9 @@ def process_all_data():
 
         # Clean up empty directories in raw_data after processing
         cleanup_empty_directories(RAW_DATA_PATH)
-    finally:
-        release_connection(connection)
+    except (psycopg2.Error, Exception) as e:
+        logger.error(f"An error occurred while processing data: {str(e)}")
+        traceback.print_exc()
 
 
 def main():
